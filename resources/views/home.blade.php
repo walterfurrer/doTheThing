af9075b8c5afdaa736d89bdf93db100ec976159e
Walter Furrer <walterfurrer@proton.me>
2025-04-16 14:11:11 -0400

    Forgot to do an earlier commit..whoops!
    Rerouted the homepage, added my own font and an image.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <header>
+        <h1 class="text-7xl font-bold">Just Start</h1>
+        <p>Find motivation to start doing the thing!</p>
+    </header>
+
+</body>
+</html>
