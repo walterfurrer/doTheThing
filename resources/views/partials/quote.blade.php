c06ee92906edb377cda5d0203633e0854f12e932
Walter Furrer <walterfurrer@proton.me>
2025-04-17 14:12:37 -0400

    Added quotes to array and display all on page

--- resources/views/partials/quote.blade.php
+++ resources/views/partials/quote.blade.php
@@ -1,6 +1,22 @@
+<?php
+
+$quotes = [
+    "Have your fun and once you have your fill, let those fingers dance on that keyboard!",
+    "If you look for balance you won't find it. Just create the balance instead, or leave things unbalanced for a bit. Once you make good progress, balance it once again.",
+    "Keep building, keep failing, keep learning. Rinse and repeat.",
+    "Execution of a task is more valuable than the knowledge to execute without th emovement behind it.",
+    "Winning satisfies hard work, but never ever stop improving your craft.",
+    "When you care too much, you push yourself for perfection. When you don't care, mediocrity is accepted.",
+    "It's not that you're not good enough, you're just not good enough yet. Turn rejection into an opportunity to learn."
+]
+
+?>
+
 <main class="place-items-center">
     <div>
-        <p class="text-left text-xl lg:text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, id quod. Aut debitis exercitationem illum itaque molestias quos rem repellat.</p>
-        <p class="text-right text-lg lg:text-xl">-Author</p>
+        @foreach ($quotes as $quote)
+            <p class="text-left text-xl lg:text-2xl">{{ $quote }}</p>
+            <br>
+        @endforeach
     </div>
 </main>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+
+    ]);
 });
