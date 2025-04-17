6d582ed00ecbe17573fa8bc8c0260faf6e01514d
Walter Furrer <walterfurrer@proton.me>
2025-04-17 10:47:16 -0400

    Split the home page into partials
    Created partials for social icons, footer, quote, and header.

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,4 @@
+<header class="place-items-center">
+    <h1 class="font-black text-center text-6xl lg:text-9xl">do the thing</h1>
+    <p class="font-semibold text-xl text-center lg:text-3xl">find motivation to just start doing the thing</p>
+</header>
---
+++ resources/views/partials/quote.blade.php
@@ -0,0 +1,6 @@
+<main class="place-items-center">
+    <div>
+        <p class="text-left text-xl lg:text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, id quod. Aut debitis exercitationem illum itaque molestias quos rem repellat.</p>
+        <p class="text-right text-lg lg:text-xl">-Author</p>
+    </div>
+</main>
