49b29d6439132ce249115a1a4eaa907384184d98
Walter Furrer <walterfurrer@proton.me>
2025-04-17 15:02:34 -0400

    Added blade ui kit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
 <body class="flex flex-col gap-32 h-dvh place-content-center mx-auto min-w-sm max-w-5xl text-pretty px-4">
     @include('partials.header')
     @include('partials.quote')
+    <button>Need more motivation?</button>
     @include('partials.footer')
 </body>
 </html>
