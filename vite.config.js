595138d3d56103c99d155d7239168c60d92d2b31
Walter Furrer <walterfurrer@proton.me>
2025-04-17 14:15:16 -0400

    Accidentally got rid of tailwind...
    I thought I was a goner.

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        }),
+        }), tailwindcss()
     ]
 });
