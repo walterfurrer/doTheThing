4dcf5775b55a15d22a2d284b1579b168de284c2b
Walter Furrer <walterfurrer@proton.me>
2025-04-22 13:52:50 -0400

    Fixed the button
    Removed the <span> and <button> per instructions from @ryandotfurrer.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,10 @@
 
     @vite('resources/css/app.css')
 </head>
-<body class="flex flex-col gap-12 place-content-center h-dvh mx-auto px-8 min-w-sm max-w-5xl text-pretty md:gap-18 lg:gap-24 lg:h-vh lg:gap-32">
+<body class="flex flex-col gap-12 place-content-center h-dvh mx-auto px-8 min-w-sm max-w-5xl text-pretty md:gap-18 lg:h-vh lg:gap-32">
     @include('partials.header')
     @include('partials.quote')
-    <span class="mx-auto">
-        <a href="/">
-            <button class="bg-zinc-700 px-4 py-2 rounded-lg duration-300 outline outline-2 outline-emerald-500 hover:bg-emerald-500 hover:outline-transparent hover:scale-115 hover:duration-300 md:px-8 py-4">need more motivation?
-            </button>
-        </a>
-    </span>
+        <a href="/" class="bg-zinc-700 mx-auto px-4 py-2 rounded-lg duration-300 outline-2 outline-emerald-600 hover:bg-emerald-600 hover:outline-transparent hover:scale-115 hover:duration-300 md:px-8 md:py-4">need more motivation?</a>
     @include('partials.footer')
 </body>
 </html>
